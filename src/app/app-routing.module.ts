7801fef691a11ff03996bdc2fd725908a4ad77b0
Simon Bison <simonpeterbison@gmail.com>
2019-03-16 19:59:06 -0500

    added methods for performing add and delete.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {FuturesComponent} from "./futures/futures.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'futures', component: FuturesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
